67464d08193ee3bba042ac1a9eac9579ae673532
Joel Alexandre <alexkhang25@yahoo.fr>
2022-12-08 17:42:48 -0600

    update header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,7 +5,11 @@ import {
   Flag,
   Play,
   ShoppingCart,
-  UserGroup
+  UserGroup,
+  ViewGrid,
+  Chat,
+  Bell,
+  ChevronDown
 } from "heroicons-react"
 import HeaderIcon from "./HeaderIcon";
 
@@ -17,26 +21,44 @@ function Header() {
           src="https://links.papareact.com/5me" 
           width={40} 
           height={40} 
-          layout="fixed"
           alt="Next Facebook"
         />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-          <Search className="h-6"/>
+          <Search className="h-6 text-gray-600"/>
           <input 
-          className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" 
-          type="text" 
-          placeholder="Search Facebook" />
+            className="
+            hidden 
+            md:inline-flex
+            ml-2 
+            items-center 
+            bg-transparent 
+            outline-none 
+            placeholder-gray-500 
+            flex-shrink" 
+            type="text" 
+            placeholder="Search Facebook" />
         </div>
       </div>
 
       <div className="center-header flex flex-grow justify-center">
-        <div className="flex space-x-6 md:space-x-6">
+        <div className="flex space-x-6 md:space-x-2">
           <HeaderIcon Icon={Home} />
           <HeaderIcon Icon={Flag} />
           <HeaderIcon Icon={Play} />
           <HeaderIcon Icon={ShoppingCart} />
           <HeaderIcon Icon={UserGroup} />
         </div>
+      </div>
+
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3">Alex Khang</p>
+        <ViewGrid className="icon" />
+        <Chat className="icon" />
+        <Bell className="icon" />
+        <ChevronDown className="icon" />
+        
       </div>
     </div>
   )
