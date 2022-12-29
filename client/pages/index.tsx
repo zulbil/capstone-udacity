4c3fb55c6bf5f8e4f2e05c3ab7c216f150be32a5
Zulbil <alexkhang25@yahoo.fr>
2022-12-28 19:50:03 -0600

    add session in the app

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,11 @@
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { GetServerSideProps } from 'next';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div>
       <Head>
@@ -16,3 +20,13 @@ export default function Home() {
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+  return {
+    props: {
+      session
+    }
+  }
+};
+
