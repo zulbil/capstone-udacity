8096ba249a30ba60c52a09ccc628a7c7ff997fe6
Zulbil <alexkhang25@yahoo.fr>
2023-01-03 21:28:05 -0600

    get accesstoken and idtoken fron next-auth

--- client/pages/api/auth/[...nextauth].tsx
+++ client/pages/api/auth/[...nextauth].tsx
@@ -4,14 +4,27 @@ import Auth0Provider from "next-auth/providers/auth0"
 export default NextAuth({
     providers: [
       Auth0Provider({
-          clientId: process.env.CLIENT_ID || '',
-          clientSecret: process.env.CLIENT_SECRET || '',
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
           issuer: process.env.AUTH0_DOMAIN
       })
     ],
     callbacks: {
+        async jwt({ token, account, profile }) {
+            if (account) {
+              token.idToken = account.id_token
+              token.accessToken = account.access_token
+              token.id = profile?.sub
+            }
+            
+            return token
+        },
         session({ session, token, user }) {
-            return session // The return type will match the one returned in `useSession()`
+            session.accessToken = token.accessToken
+            session.idToken = token.idToken
+            session.user.id = token.id
+
+            return session 
         }
     }
 })
