6f27caccb712cc605070d667648496a847dc8eb9
Zulbil <alexkhang25@yahoo.fr>
2023-01-27 09:56:24 -0600

    remove the AuthContextProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,16 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import { AuthContextProvider } from '../contexts/AuthContext'
+import { Session } from 'next-auth'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps<{session : Session}>) {
   return (
-    <AuthContextProvider>
       <SessionProvider
         session={pageProps.session}
       >
         <Component {...pageProps} />
       </SessionProvider>
-    </AuthContextProvider>
   )
 }
