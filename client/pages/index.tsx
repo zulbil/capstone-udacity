5446508f9411a9bb1a700b40b74e551653f141dc
Zulbil <alexkhang25@yahoo.fr>
2023-01-27 10:08:48 -0600

    update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,23 +6,13 @@ import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import { Session } from 'next-auth';
-import { getPosts } from '../services/postService';
-import { Post } from '../types/Post';
 import Widgets from '../components/Widgets';
-import { useAuthContext } from '../contexts/AuthContext';
-import { useEffect, useState } from 'react';
 
 interface HomeProps {
   session: Promise<Session>
 }
 
 export default function Home({ session } : HomeProps) {
-
-  const { authData, setAuthData } = useAuthContext()
-
-  useEffect(()=> {
-    setAuthData({ ...authData, session })
-  }, [])
 
   if (!session) return <Login />
   return (
